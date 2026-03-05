e96984b9b85859f55dad727594d51465045e42c4
ll06 <ll06printf@outlook.com>
2026-03-05 16:46:13 +0800

    Build with GCC
    - Try to build NovaPhy with GCC. The compatibility state is recorded within build.md.
    - Use $VCPKG_ROOT as vcpkg repository directroy
    - Add script used to build NovaPhy by individual GCC/CUDA  installation

---
+++ compat/build.md
@@ -0,0 +1,86 @@
+# Build NovaPhy
+
+## Build on Windows with MSVC
+
+TODO
+
+## Build on Linux with GCC/Clang
+
+### Basic
+
+NovaPhy without IPC support just needs the following prerequisites (as mentioned in [README.md](../README.md)):
+
+- [Conda](https://docs.conda.io/) (Miniconda or Anaconda)
+- [vcpkg](https://vcpkg.io/) installed. *Optional*
+- C++17 compiler
+  - MSVC 2019+
+  - GCC 9+
+  - Clang 10+
+
+```bash
+# Create conda environment
+conda env create -f environment.yml
+conda activate novaphy
+
+# Install NovaPhy
+pip install -e .
+
+# Then you can access NovaPhy from Python in the virtual environment.
+python
+```
+
+### IPC support
+
+NovaPhy relies on [libuipc](https://github.com/spiriMirror/libuipc) for IPC support.
+It requires C++20 features, and its upstream depends on some non-standard features.
+Thus, the prerequisites are more specific.
+
+- [Conda](https://docs.conda.io/) (Miniconda or Anaconda)
+- [vcpkg](https://vcpkg.io/) installed.
+- C++20 compiler
+  - MSVC 2019+
+  - GCC 11+
+  - Clang 10+
+- CUDA 12.4
+
+```bash
+conda activate novaphy
+
+# Before this step, make sure your environment variables are set, such as VCPKG_ROOT.
+CMAKE_ARGS="--preset=ipc" pip install -e .
+```
+
+### Troubleshooting
+
+If you use a compiler not listed above, it may fail with the default configuration.
+Here are some common reasons for crashes with specific compilers:
+
+| Compiler | Reason | Status |
+|:---:|:---|:---:|
+| `gcc-9` | **libuipc** needs `<span>` which was implemented in GCC 10. | Unfixable |
+| `gcc-10` | The pstl of `libstdc++ 10` isn't compatible with `onetbb` in the current baseline. (See also [vcpkg.json](../vcpkg.json)). | Unfixable |
+| almost all GCC tested | `urdfdom` expects a non-standard import for `uint32_t` which is removed from `libstdc++` (may be earlier). | TODO |
+| `gcc14, gcc-15` | `gcc>13` is not compatible with `nvcc-12` | Unfixable |
+| `nvcc-13` | **muda** expects an unstrict dependent name lookup. | TODO |
+
+> [!note] Clang with libstdc++
+> Clang uses `libstdc++` as the default standard library. Any crash caused by `libstdc++` also affects Clang.
+
+Compilers are described by name and major version, such as `gcc-9`. For each major version, only one version is tested:
+
+- GCC 9.5.0
+- GCC 10.5.0
+- ✅ GCC 11.5.0
+- ✅ GCC 12.5.0
+- ✅ GCC 13.4.0
+- GCC 14.3.0
+- GCC 15.2.1
+- ✅ nvcc-12: Cuda compilation tools, release 12.4, V12.4.131
+- nvcc-13: Cuda compilation tools, release 13.1, V13.1.115
+
+#### Shared library linkage
+
+If you run a demo, for example, `demos/demo_ipc_stack.py`, you will get a dynamic linkage error when IPC is enabled.
+Just making the *.so files available in your linking path (by copying the files to your library path, or by adding their directory to `LD_LIBRARY_PATH`) will not solve the problem. Some libraries are imported using relative paths, so you need to ensure your current working directory contains these library files.
+
+TODO: This behavior is unexpected. CMake install configuration may be needed.
---
+++ compat/script/build-linux-gcc.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import sys
+import subprocess
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Build NovaPhy with GCC compiler",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    
+    parser.add_argument(
+        '--gcc-dir',
+        type=str,
+        default=None,
+        help='Path to GCC installation directory',
+        required=False
+    )
+    
+    parser.add_argument(
+        '--cuda-dir',
+        type=str,
+        default=None,
+        help='Path to CUDA installation directory',
+        required=False
+    )
+
+    parser.add_argument(
+        '--ipc',
+        action='store_true',
+        help='enable IPC support'
+    )
+
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output for debugging"
+    )
+
+    parser.add_argument(
+        "--build-dir",
+        type=str,
+        default=r"build/{wheel_tag}",
+        help="Directory to store build files"
+    )
+    
+    args = parser.parse_args()
+    
+    # Process arguments
+    gcc_dir = args.gcc_dir
+    cuda_dir = args.cuda_dir
+    enable_ipc = args.ipc
+    verbose_flag = args.verbose
+    build_dir = args.build_dir
+    
+    print(f"GCC Directory: {gcc_dir if gcc_dir else 'Not specified'}")
+    print(f"CUDA Directory: {cuda_dir if cuda_dir else 'Not specified'}")
+    
+    # Select preset based on IPC option
+    if enable_ipc:
+        cmake_args = ["--preset=ipc"]
+
+        # Add header flag for IPC support
+        # TODO Need another method to process urdfdom.
+        cxxflags = os.environ.get("CXXFLAGS", "")
+        os.environ["CXXFLAGS"] = (cxxflags + " -include cstdint").strip()
+    else:
+        cmake_args = ["--preset=default"]
+
+    if gcc_dir:
+        gcc_path = os.path.join(gcc_dir, "bin", "gcc")
+        gpp_path = os.path.join(gcc_dir, "bin", "g++")
+        cmake_args.append(f"-DCMAKE_C_COMPILER={gcc_path}")
+        cmake_args.append(f"-DCMAKE_CXX_COMPILER={gpp_path}")
+        os.environ["CC"] = gcc_path
+        os.environ["CXX"] = gpp_path
+    else:
+        # Use GCC in PATH
+        os.environ["CC"] = "gcc"
+        os.environ["CXX"] = "g++"
+
+    if cuda_dir:
+        nvcc_path = os.path.join(cuda_dir, "bin", "nvcc")
+        cmake_args.append(f"-DCMAKE_CUDA_COMPILER={nvcc_path}")
+        if gcc_dir:
+            cmake_args.append(f"-DCMAKE_CUDA_HOST_COMPILER={gpp_path}")
+            cuda_include = os.path.join(cuda_dir, "include")
+            gcc_include = os.path.join(gcc_dir, "include")
+            nvcc_flags = f"--system-include {cuda_include},{gcc_include}"
+            cmake_args.append(f"-DCMAKE_CUDA_FLAGS=\"{nvcc_flags}\"")
+
+    os.environ["CMAKE_BUILD_PARALLEL_LEVEL"] = "16"
+    print("Running CMake with arguments:", " ".join(cmake_args))
+    os.environ["CMAKE_ARGS"] = " ".join(cmake_args)
+
+    print("🧰Build configuration summary")
+    print("CMAKE_ARGS: ", os.environ["CMAKE_ARGS"])
+    print("CC:\t", os.environ["CC"])
+    print("CXX:\t", os.environ["CXX"])
+
+    cmd = ["pip", "install", "-e", ".", "-C", f"build-dir={build_dir}"]
+    if (verbose_flag):
+        cmd.append("-v")
+    subprocess.run(cmd, check=True)
+
+
+if __name__ == '__main__':
+    main()
